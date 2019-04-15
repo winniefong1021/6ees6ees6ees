0154cc307f86c0783a2727a7a461b198a2a0fea5
Winnie Fong <winniefong21@gmail.com>
2019-04-15 12:11:54 -0700

    Implement HoneyMakerBee as subclass of Bee with makeHoney and giveHoney methods

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,27 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor() {
+    super()
+    this.age = 10
+    this.job = 'make honey'
+    this.honeyPot = 0
+  }
+
+  makeHoney() {
+    this.honeyPot++
+  }
+
+  giveHoney() {
+    this.honeyPot--;
+  }
 };
+
+// Create a HoneyMakerBee class, in ES6 style, with:
+// the Bee superclass
+// an age property that is set to 10
+// a job property that is set to make honey
+// a color property inherited from bee that is set to yellow
+// a food property that is inherited from grub
+// an eat method that is inherited from grub
+// a honeyPot property that is set to 0
+// a makeHoney method that adds 1 to that honeyBee's honeyPot
+// a giveHoney method that subtracts 1 from that honeyBee's honeyPot
